3d4e13e450b583d1251e10c2fb87983886e5224c
naka <s.ohara@infwo.sumomo.ne.jp>
2015-09-09 19:16:24 +0900

    graph test

--- app/src/main/java/com/example/so/testapp/CircleGraphView.java
+++ app/src/main/java/com/example/so/testapp/CircleGraphView.java
@@ -1,4 +1,219 @@
 package com.example.so.testapp;
 
-public class CircleGraphView {
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.SurfaceTexture;
+import android.util.Log;
+import android.view.TextureView;
+
+import java.util.ArrayList;
+
+public class CircleGraphView extends TextureView implements TextureView.SurfaceTextureListener,Runnable{
+
+
+    static final long FPS = 30;
+    static final long FRAME_TIME = 600 / FPS;
+    Thread thread;
+
+    //グラフ描画領域のパディング
+    private int leftpadding = 30;
+    private int rightpadding = 30;
+    private int toppadding = 30;
+    private int bottompadding = 30;
+
+    //レイアウトされたViewのサイズ
+    private int view_w;
+    private int view_h;
+
+    //グラフにするデータセット
+    private ArrayList<Float> datasetList;
+    private int datalistcount;
+
+    //軸のスケール設定
+    private float scale_x;
+    private float scale_y;
+    private float max_y;
+
+    public CircleGraphView(Context context){
+        super(context);
+        setSurfaceTextureListener(this);
+    }
+
+
+    @Override
+    public void onSurfaceTextureAvailable(SurfaceTexture surface, int w,
+                                          int h) {
+
+        view_h = h;
+        view_w = w;
+
+        Log.v("log", String.format("onSurfaceTextureAvailable: %s %s", w, h));
+
+        synchronized (this) {
+            thread = new Thread(this);
+            thread.start();
+        }
+    }
+    @Override
+    public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+        synchronized (this) {
+            thread.interrupt();
+            thread = null;
+        }
+        return false;
+    }
+
+    @Override
+    public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int w,
+                                            int h) {
+        Log.v("log", String.format("onSurfaceTextureSizeChanged: %s %s", w, h));
+    }
+    @Override
+    public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+    }
+
+    @Override
+    public void run() {
+        Canvas canvas = null;
+
+        long loopCount = 0;
+        long waitTime = 0;
+        long startTime = System.currentTimeMillis();
+
+        Paint paint = new Paint();
+        Paint bgPaint = new Paint();
+
+        Path path = new Path();
+
+        Path pathline = new Path();
+
+
+        float phase = 0;
+
+        // Background
+        bgPaint.setStyle(Paint.Style.FILL);
+        bgPaint.setColor(Color.WHITE);
+
+
+        // 開始位置
+        pathline.moveTo(datasetList.get(0) +  leftpadding
+                , datasetList.get(0) - bottompadding + view_h);
+
+        //x軸のスケールをセット
+        scale_x = (view_w - rightpadding - leftpadding)/(float)datalistcount;
+
+        //y軸のスケールをセット
+        scale_y = view_h/max_y;
+
+        //アニメーション用のループ
+        while (thread != null) {
+
+            try {
+                loopCount++;
+
+                synchronized (this) {
+                    canvas = this.lockCanvas();
+
+                    /**
+                     * フレーム描画
+                     */
+                    //グラフラインのスタイル
+                    paint.setAntiAlias(true);
+                    paint.setStyle(Paint.Style.STROKE); // スタイルは線(Stroke)を指定する
+                    paint.setStrokeWidth(3); // 線の太さ
+                    paint.setColor(Color.rgb(255, 128, 0)); // 線の色
+
+                    //背景の適用
+                    canvas.drawPaint(bgPaint);
+
+                    //軸の描画
+                    path.moveTo(leftpadding, toppadding);
+                    path.rLineTo(0, view_h - toppadding - bottompadding);
+                    path.rLineTo(view_w - rightpadding - leftpadding, 0);
+                    //path.rLineTo(0, -view_h + toppadding + bottompadding);
+                    canvas.drawPath(path, paint);
+                    paint.reset();
+
+                    //ラベルの描画
+                    paint.setAntiAlias(true);
+                    paint.setColor(Color.rgb(255, 128, 0)); // 線の色
+                    paint.setTextSize(25);
+                    float textpos = (paint.getFontMetrics().bottom-paint.getFontMetrics().ascent)/2f;
+                    float textXpos = (paint.measureText("Test1"))/2f;
+                    Log.v("log", String.format("Text h : %s ", textpos));
+
+                    canvas.drawText("Test1",0, toppadding + textpos,paint);
+                    canvas.drawText("Test2",0, (view_h-bottompadding-toppadding)/2 + textpos,paint);
+                    canvas.drawText("Test3",0, view_h-bottompadding-toppadding + textpos,paint);
+                    canvas.drawText("Test4",textXpos, view_h,paint);
+                    canvas.drawText("Test5",(view_w-leftpadding-rightpadding)/2+textXpos, view_h,paint);
+                    canvas.drawText("Test5",view_w-leftpadding-rightpadding+textXpos, view_h,paint);
+
+                    path.reset();
+                    paint.reset();
+                    /**
+                     * グラフプロット
+                     * アニメーション
+                     */
+                    //グラフラインのスタイル
+                    paint.setAntiAlias(true);
+                    paint.setStyle(Paint.Style.STROKE); // スタイルは線(Stroke)を指定する
+                    paint.setStrokeWidth(6); // 線の太さ
+                    paint.setColor(Color.rgb(0, 128, 255)); // 線の色
+                    paint.setStrokeCap(Paint.Cap.ROUND);
+                    paint.setStrokeJoin(Paint.Join.ROUND);
+
+
+                    pathline.quadTo((float) phase*scale_x +  leftpadding, (float) -datasetList.get((int)phase)*scale_y - bottompadding + view_h
+                            , (float) (phase+1)*scale_x +  leftpadding, (float) -datasetList.get((int)(phase + 1))*scale_y - bottompadding + view_h);
+
+                    phase = phase+1;
+
+                    canvas.drawPath(pathline, paint);
+
+                    paint.setPathEffect(null);
+
+                    this.unlockCanvasAndPost(canvas);
+
+                }
+
+                waitTime = (loopCount * FRAME_TIME)
+                        - (System.currentTimeMillis() - startTime);
+
+
+                if (phase>datalistcount -2 ){
+
+                    Log.v("log", String.format("Last  X : %s ", (phase+1)*scale_x));
+                    break;
+                }
+
+                if (waitTime > 0) {
+                    Thread.sleep(waitTime);
+                }
+            } catch (Exception e) {
+                //エラー時、unlockする
+                //this.unlockCanvasAndPost(canvas);
+                break;
+            }
+
+
+        }
+    }
+
+    /**
+     * データリストをセット
+     * @param datasetList
+     */
+    public void setDatasetList(ArrayList<Float> datasetList) {
+        this.datasetList = datasetList;
+        this.datalistcount = datasetList.size();
+    }
+
+    public void setScale_y(float maxy){
+        this.max_y = maxy;
+    }
 }
+
--- app/src/main/java/com/example/so/testapp/TabUiTest.java
+++ app/src/main/java/com/example/so/testapp/TabUiTest.java
@@ -4,6 +4,7 @@ import android.app.Fragment;
 import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentTabHost;
 import android.util.Log;
@@ -20,15 +21,24 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.PolygonOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.maps.android.PolyUtil;
+
+import java.util.ArrayList;
 
 public class TabUiTest extends Fragment {
 
 
-    private LinearLayout rootView;
+    private RelativeLayout rootView;
     private Context context;
     private GoogleMap googleMap;
 
     private TabLayout tabLayout;
+
+    private Polyline polyline1,polyline2,polyline3,polyline4;
+    private ArrayList<Polyline> ArrayPolylineSet;
 
     public TabUiTest(){
 
@@ -39,17 +49,19 @@ public class TabUiTest extends Fragment {
 
         context = getActivity().getApplicationContext();
 
+        ArrayPolylineSet = new ArrayList<Polyline>();
+
         //root
-        rootView = (LinearLayout)inflater.inflate(R.layout.fragment_tab, container, false);
+        rootView = (RelativeLayout)inflater.inflate(R.layout.fragment_tab, container, false);
         rootView.setOnClickListener(null);
         rootView.setBackgroundColor(Color.argb(255, 255, 255, 255));
 
         tabLayout = (TabLayout) rootView.findViewById(R.id.tab_layout);
         tabLayout.setBackgroundColor(Color.BLACK);
         tabLayout.setTabTextColors(Color.WHITE, Color.YELLOW);
-        tabLayout.addTab(tabLayout.newTab().setText("Tab 1").setTag("1"));
-        tabLayout.addTab(tabLayout.newTab().setText("Tab 2").setTag("2"));
-        tabLayout.addTab(tabLayout.newTab().setText("Tab 3").setTag("3"));
+        tabLayout.addTab(tabLayout.newTab().setText("陣取りゲーム").setTag("1"));
+        tabLayout.addTab(tabLayout.newTab().setText("情報").setTag("2"));
+        tabLayout.addTab(tabLayout.newTab().setText("ウォーキング").setTag("3"));
 
 
         //Goomap インスタンス取得
@@ -61,21 +73,84 @@ public class TabUiTest extends Fragment {
             public void onMapReady(GoogleMap gMap) {
                 googleMap = gMap;
 
+                //初期位置
+                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(35.694922, 139.705361), 11));
+
+                //ポリラインの描画
+                PolylineOptions rectOptions = new PolylineOptions()
+                        .add(new LatLng(35.687894, 139.676166),
+                                new LatLng(35.556467, 139.643636),
+                                new LatLng(35.548171, 139.614504));
+                rectOptions.color(Color.BLUE);
+                rectOptions.width(5);
+                polyline1 = googleMap.addPolyline(rectOptions);
+
+                rectOptions = new PolylineOptions()
+                        .add(new LatLng(35.715888, 139.745597),
+                                new LatLng(35.645293, 139.773273),
+                                new LatLng(35.575425, 139.741713));
+                rectOptions.color(Color.BLUE);
+                rectOptions.width(5);
+                polyline2 = googleMap.addPolyline(rectOptions);
+
+                rectOptions = new PolylineOptions()
+                        .add(new LatLng(35.717465, 139.599938),
+                                new LatLng(35.636217, 139.629070),
+                                new LatLng(35.535924, 139.625185));
+                rectOptions.color(Color.BLUE);
+                rectOptions.width(5);
+                polyline3 = googleMap.addPolyline(rectOptions);
+
+                rectOptions = new PolylineOptions()
+                        .add(new LatLng(35.740326, 139.569835),
+                                new LatLng(35.647660, 139.564494),
+                                new LatLng(35.569106, 139.592169));
+                rectOptions.color(Color.BLUE);
+                rectOptions.width(5);
+                polyline4 = googleMap.addPolyline(rectOptions);
+
+                ArrayPolylineSet.add(polyline1);
+                ArrayPolylineSet.add(polyline2);
+                ArrayPolylineSet.add(polyline3);
+                ArrayPolylineSet.add(polyline4);
+
+                googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+                    @Override
+                    public void onMapClick(LatLng latLng) {
+                        //ポリラインの選択状態をリセット
+                        for(Polyline tarline : ArrayPolylineSet){
+                                tarline.setColor(Color.BLUE);
+                        }
+
+
+                        //ポリラインのタップを検知
+                        for(Polyline tarline : ArrayPolylineSet){
+                            if (PolyUtil.isLocationOnPath(latLng, tarline.getPoints(), true, 100)) {
+                                tarline.setColor(Color.rgb(255,0,255));
+                                break;
+                            }
+                        }
+                    }
+                });
+
                 tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
                     @Override
                     public void onTabSelected(TabLayout.Tab tab) {
 
-                        switch(tab.getPosition()){
+                        switch (tab.getPosition()) {
                             case 0:
-                                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(35.630062, 139.733086), 15));
+                                //googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(35.630062, 139.733086), 11));
+                                displayAllLine();
                                 break;
 
                             case 1:
-                                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(35.679704, 139.697616), 15));
+                                //googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(35.679704, 139.697616), 11));
+                                displayAllLine();
                                 break;
 
                             case 2:
-                                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(35.683915, 139.765614), 15));
+                                //googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(35.683915, 139.765614), 11));
+                                displaySelectedLine();
                                 break;
                         }
 
@@ -95,8 +170,21 @@ public class TabUiTest extends Fragment {
         });
 
 
+        /**
+         * Floating Action Button
+         * Android Design support library
+         */
+
+        /*FloatingActionButton fab = new FloatingActionButton(context);
+        fab.setBackgroundDrawable(null);
+        fab.setBackgroundColor(Color.CYAN);
+        fab.getBackgroundTintList();
+        fab.setRippleColor(Color.GREEN);
+        fab.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT
+                                    ,LinearLayout.LayoutParams.MATCH_PARENT));
 
 
+        rootView.addView(fab);*/
 
         return rootView;
     }
@@ -113,6 +201,20 @@ public class TabUiTest extends Fragment {
 
     }
 
+    private void displaySelectedLine(){
+        for(Polyline tarline : ArrayPolylineSet){
+            if(tarline.getColor() == Color.rgb(255,0,255)){
+                tarline.setVisible(true);
+            }else{
+                tarline.setVisible(false);
+            }
+        }
+    }
 
+    private void displayAllLine(){
+        for(Polyline tarline : ArrayPolylineSet){
+                tarline.setVisible(true);
+        }
+    }
 
 }
